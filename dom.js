a7eb9ff03793342b370c5929c7cd1a9508ef119a
clement ng <cgng16@gmail.com>
2018-07-31 09:26:08 -0700

    dom stuff

---
+++ dom.js
@@ -0,0 +1,20 @@
+/*var body2 = document.querySelector("body");
+var ispink = false;
+
+var h1 = document.querySelector("h1");
+h1.style.color = "pink";
+
+setInterval(function(){
+    if(ispink){
+        body2.style.background = "green";
+    }else{
+        body2.style.background = "white";
+    }
+    ispink = !ispink;
+}, 1000);
+*/
+
+var p1 = document.getElementById("first");
+var p2 = document.getElementsByClassName("special")[0];
+var p3 = document.querySelector("p");
+var p4 = document.querySelector(".special");
